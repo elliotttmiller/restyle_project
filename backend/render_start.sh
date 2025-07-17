c481f32ba509893465748b0e9c74a36470d6feac
Elliott Miller <elliotttmiller@hotmail.com>
2025-07-17 10:39:35 -0500

    production settings, app config fixes

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+pip install -r requirements.txt
 python manage.py test
 python manage.py migrate
 python create_cloud_superuser.py
+python manage.py collectstatic --no-input
 gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT 
