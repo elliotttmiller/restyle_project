4ebaf0a01708cd24f9dac6d26fcc477c5ba6b2d9
Elliott Miller <elliotttmiller@hotmail.com>
2025-07-17 11:24:48 -0500

    production: remove tests

--- backend/render_start.sh
+++ backend/render_start.sh
@@ -2,8 +2,6 @@
 set -e
 
 pip install -r requirements.txt
-python manage.py test
 python manage.py migrate
-python create_cloud_superuser.py
-python manage.py collectstatic --no-input
-gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT 
+python manage.py create_cloud_superuser.py
+python manage.py collectstatic --no-input 
